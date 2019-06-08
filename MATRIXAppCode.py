694f8678cdd657aa3a3e40f932e89d04f18181a0
bill Li <billli@matrix.space>
2019-06-08 18:47:45 +0800

    add ui files

---
+++ MATRIXAppCode.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing MATRIXAppCode.
+"""
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import *
+
+from Ui_MATRIXWallet import Ui_MainWindow
+
+from random import randint
+
+import sys
+
+import re
+
+class MATRIXAppCode(QMainWindow, Ui_MainWindow):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+        
+        @param parent reference to the parent widget
+        @type QWidget
+        """
+        super(MATRIXAppCode, self).__init__(parent)
+        self.setupUi(self)
+        self.num = randint(1,100)
+
+        self.show()
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, '确认', '确认退出吗', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+         
+    
+    
+    @pyqtSlot()
+    def on_pushButton_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        #QMessageBox.about(self,self.lineEdit.text(), 'Good!')
+        inputstr1=self.lineEdit.text()
+        inputstr=inputstr1.strip()
+       
+        regPattern= 'r\'^MAN\.[a-km-zA-HJ-NP-Z1-9]{25,34}\''
+        #regPattern= '''r'^MAN\.[a-km-zA-HJ-NP-Z1-9]{25,34}\''''   
+        #regPattern=r'^https?:/{2}\w.+$'
+        #pattern = re.compile(regPattern) #This method is not ok!
+        pattern = re.compile(r'^MAN\.[a-km-zA-HJ-NP-Z1-9]{25,34}')
+ 
+        # 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
+        match = pattern.match(inputstr)
+         
+        if match:
+            # 使用Match获得分组信息
+            print("Ok.")
+            QMessageBox.question(self, '提示部署输入账户为' ,inputstr,  QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        else:
+            QMessageBox.about(self, '提示请勿输入A0账户','请输入正确的MATRIX A1账户')
+
+            self.num = randint(1,100)
+
+            self.lineEdit.clear()
+
+            self.lineEdit.setFocus()
+        #guessnumber = 10 
+        #int(self.lineEdit.text())
+
+        print(self.num)
+        #raise NotImplementedError
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ui = MATRIXAppCode()
+   
+    sys.exit(app.exec_())
---
+++ Ui_go.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/Users/liqinghua/eric/test2/go.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../Downloads/ico/Dialog.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MATRIX Depoly tool"))
+        self.pushButton.setToolTip(_translate("MainWindow", "<b>MATRIX Signal </b>"))
+        self.pushButton.setText(_translate("MainWindow", "点击生效"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
